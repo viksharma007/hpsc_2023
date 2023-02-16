5c1b2fb1834bb6fa104d3889bc2ed29851845df9
Vikrant Sharma <s22016@studnets.iitmandi.ac.in>
2023-02-16 16:49:57 +0530

    Update square root function to take negative and 0 value

--- codes/python/mysqrt/mysqrt.py
+++ codes/python/mysqrt/mysqrt.py
@@ -8,6 +8,14 @@ def sqrtNT(x,debug=False):
     The actual sqrtNT function
     Takes one input x: the number whose square root is to be calculated. 
     '''
+    from numpy import nan
+    
+    if x==0. :
+    	return 0.
+    elif x<0. :
+    	print("An error has occured since you have passed a negatice value which does not have real square root")
+    	return nan
+    assert x>0., "Input not recignised"
     s = 1.0
     kmax = 100
     tol = 1.0e-14
