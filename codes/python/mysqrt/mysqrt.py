13056fa09e2409d752aaff17081a286190d8e933
Vikrant Sharma <s22016@studnets.iitmandi.ac.in>
2023-02-16 16:07:42 +0530

    Updated the squareroot file

--- codes/python/mysqrt/mysqrt.py
+++ codes/python/mysqrt/mysqrt.py
@@ -3,19 +3,22 @@ A python module for calculating square root using the Newton's method implemente
 '''
 
 
-def sqrtNT(x):
-	'''
-	The actual sqrtNT function
-	Takes one input x: the number whose square root is to be calculated 
-	'''
-	s = 1.0
-	kmax = 100
-	tol = 1.0e-14
-	for k in range(kmax):
-    		print("Iteration number %s, s = %20.15f" %(k,s))
-    		s0 = s
-    		s = 0.5*(s + x/s)
-    		delta_s = s - s0
-    		if (abs(delta_s)/x)<tol:
-    				break
-	print("After %s number of iteration ,s = %20.15f" %(k+1,s))
+def sqrtNT(x,debug=False):
+    '''
+    The actual sqrtNT function
+    Takes one input x: the number whose square root is to be calculated. 
+    '''
+    s = 1.0
+    kmax = 100
+    tol = 1.0e-14
+    for k in range(kmax):
+        if debug: 
+            print("At Iteration number %s, s = %20.15f" %(k,s))
+        s0=s
+        s = 0.5*(s + x/s)
+        delta_s = s - s0
+        if (abs(delta_s)/x)<tol:
+             break
+    if debug:
+        print("After %s number of iteration ,s = %20.15f" %(k+1,s))
+    return s
